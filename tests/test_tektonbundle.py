a4b1f2d840f1408dde502a2fc492c6663e03b5a2
Chmouel Boudjnah <chmouel@chmouel.com>
2020-10-20 20:03:06 +0200

    Add tests and some TDD tests 💘
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- tests/test_tektonbundle.py
+++ tests/test_tektonbundle.py
@@ -1,24 +1,148 @@
 #!/usr/bin/env python
-
-"""Tests for `tektonbundle` package."""
-
 import pytest
-
+import yaml
 
 from tektonbundle import tektonbundle
+"""Tests for `tektonbundle` package."""
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+def test_with_taskspec():
+    """Expands pipeline but keep the taskSpec"""
+    output = yaml.safe_load(
+        tektonbundle.parse([
+            """---
+    apiVersion: tekton.dev/v1beta1
+    kind: PipelineRun
+    metadata:
+      name: pr-test1
+    spec:
+      pipelineRef:
+        name: pipeline-test1
+      params:
+        - name: key
+          value: {{value}}
+        - name: revision
+          value: "bar"
+    """, """---
+    apiVersion: tekton.dev/v1beta1
+    kind: Pipeline
+    metadata:
+      name: pipeline-test1
+    spec:
+      params:
+        - name: repo_url
+        - name: revision
+      tasks:
+        - name: task-of-pipeline-test1
+          taskSpec:
+            steps:
+            - name: first-step
+              image: image1
+        - name: task-of-pipeline-test2
+          taskRef:
+            name: task-test2
+      steps:
+        - name: first-step
+          image: image
+    """, """---
+    apiVersion: tekton.dev/v1beta1
+    kind: Task
+    metadata:
+      name: task-test2
+    spec:
+      steps:
+        - name: second-step
+          image: image
+"""
+        ], {"value": "hello"}))
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+    assert output['spec']['pipelineSpec']['tasks'][0]['taskSpec']['steps'][0][
+        'name'] == "first-step"
+    assert output['spec']['pipelineSpec']['tasks'][1]['taskSpec']['steps'][0][
+        'name'] == "second-step"
+
+
+def test_simple():
+    output = yaml.safe_load(
+        tektonbundle.parse([
+            """---
+    apiVersion: tekton.dev/v1beta1
+    kind: PipelineRun
+    metadata:
+      name: pr-test1
+    spec:
+      pipelineRef:
+        name: pipeline-test1
+      params:
+        - name: key
+          value: {{value}}
+        - name: revision
+          value: "bar"
+    """, """---
+    apiVersion: tekton.dev/v1beta1
+    kind: Pipeline
+    metadata:
+      name: pipeline-test1
+    spec:
+      params:
+        - name: repo_url
+        - name: revision
+      tasks:
+        - name: task-of-pipeline-test1
+          taskRef:
+            name: task1
+    """, """---
+    apiVersion: tekton.dev/v1beta1
+    kind: Task
+    metadata:
+      name: task1
+    spec:
+      steps:
+        - name: first-step
+          image: image
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+        ], {"value": "hello"}))
+
+    assert 'pipelineSpec' in output['spec']
+    assert 'taskSpec' in output['spec']['pipelineSpec']['tasks'][0]
+    assert output['spec']['pipelineSpec']['tasks'][0][
+        'name'] == 'task-of-pipeline-test1'
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+@pytest.mark.skip(
+    reason="We do not handle pipelines where we have nothing to do yet.")
+def test_pipeline_nothing_todo():
+    output = yaml.safe_load(
+        tektonbundle.parse([
+            """---
+    apiVersion: tekton.dev/v1beta1
+    kind: PipelineRun
+    metadata:
+      name: pr-test1
+    spec:
+      pipelineRef:
+        spec:
+          name: pipeline
+        """
+        ], {"1": "2"}))
+    assert len(output) == 1
+
+
+@pytest.mark.skip(reason="This is failing for now.")
+def test_kubernetes_document():
+    output = yaml.safe_load(
+        tektonbundle.parse([
+            """---
+        key: value
+        """, """---
+    apiVersion: tekton.dev/v1beta1
+    kind: PipelineRun
+    metadata:
+      name: pr-test1
+    spec:
+      pipelineRef:
+        spec:
+          name: pipeline
+        """
+        ], {"1": "2"}))
+    assert len(output) == 1
