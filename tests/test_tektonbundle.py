1fc204878e321bc31aa08c31a30b3f3270cecd69
Chmouel Boudjnah <chmouel@chmouel.com>
2020-10-21 11:41:37 +0200

    Rework tests to use yamls and improve them 👨🏾‍⚖️

--- tests/test_tektonbundle.py
+++ tests/test_tektonbundle.py
@@ -1,112 +1,58 @@
 #!/usr/bin/env python
+"""Tests for `tektonbundle` package."""
+import glob
+import os
+
 import pytest
 import yaml
 
 from tektonbundle import tektonbundle
-"""Tests for `tektonbundle` package."""
 
 
-def test_with_taskspec():
-    """Expands pipeline but keep the taskSpec"""
+def get_key(dico, key):
+    curr = dico
+    for k in key.split("."):
+        if k.isdigit() and isinstance(curr, list):
+            curr = curr[int(k)]
+            continue
+        if k not in curr:
+            return ""
+        curr = curr[k]
+
+    if not isinstance(curr, str):
+        curr = str(curr)
+    return curr
+
+
+@pytest.fixture(scope="session")
+def fixtures():
+    ret = {}
+    for fname in glob.glob(
+            os.path.join(os.path.dirname(__file__), 'yamls/*.y*ml')):
+        ret[os.path.splitext(os.path.basename(fname))[0]] = fname
+    return ret
+
+
+def test_pipelinerun_pipeline_task(fixtures):
     output = yaml.safe_load(
-        tektonbundle.parse([
-            """---
-    apiVersion: tekton.dev/v1beta1
-    kind: PipelineRun
-    metadata:
-      name: pr-test1
-    spec:
-      pipelineRef:
-        name: pipeline-test1
-      params:
-        - name: key
-          value: {{value}}
-        - name: revision
-          value: "bar"
-    """, """---
-    apiVersion: tekton.dev/v1beta1
-    kind: Pipeline
-    metadata:
-      name: pipeline-test1
-    spec:
-      params:
-        - name: repo_url
-        - name: revision
-      tasks:
-        - name: task-of-pipeline-test1
-          taskSpec:
-            steps:
-            - name: first-step
-              image: image1
-        - name: task-of-pipeline-test2
-          taskRef:
-            name: task-test2
-      steps:
-        - name: first-step
-          image: image
-    """, """---
-    apiVersion: tekton.dev/v1beta1
-    kind: Task
-    metadata:
-      name: task-test2
-    spec:
-      steps:
-        - name: second-step
-          image: image
-"""
-        ], {"value": "hello"}))
+        tektonbundle.parse([fixtures['pipelinerun-pipeline-task']],
+                           {"value": "replaced_value"}))
+    assert get_key(
+        output,
+        "spec.pipelineSpec.tasks.0.taskSpec.steps.0.name"), "first-steps"
 
-    assert output['spec']['pipelineSpec']['tasks'][0]['taskSpec']['steps'][0][
-        'name'] == "first-step"
-    assert output['spec']['pipelineSpec']['tasks'][1]['taskSpec']['steps'][0][
-        'name'] == "second-step"
+    assert get_key(
+        output,
+        "spec.pipelineSpec.tasks.1.taskSpec.steps.0.name"), "second-step"
+
+    assert get_key(output, "spec.params.0.value"), "replaced_value"
 
 
-def test_simple():
+def test_unknown_template_not_replacing(fixtures):
     output = yaml.safe_load(
-        tektonbundle.parse([
-            """---
-    apiVersion: tekton.dev/v1beta1
-    kind: PipelineRun
-    metadata:
-      name: pr-test1
-    spec:
-      pipelineRef:
-        name: pipeline-test1
-      params:
-        - name: key
-          value: {{value}}
-        - name: revision
-          value: "bar"
-    """, """---
-    apiVersion: tekton.dev/v1beta1
-    kind: Pipeline
-    metadata:
-      name: pipeline-test1
-    spec:
-      params:
-        - name: repo_url
-        - name: revision
-      tasks:
-        - name: task-of-pipeline-test1
-          taskRef:
-            name: task1
-    """, """---
-    apiVersion: tekton.dev/v1beta1
-    kind: Task
-    metadata:
-      name: task1
-    spec:
-      steps:
-        - name: first-step
-          image: image
-    """
-        ], {"value": "hello"}))
-
-    assert 'pipelineSpec' in output['spec']
-    assert 'taskSpec' in output['spec']['pipelineSpec']['tasks'][0]
-    assert output['spec']['pipelineSpec']['tasks'][0][
-        'name'] == 'task-of-pipeline-test1'
+        tektonbundle.parse([fixtures['pipelinerun-pipeline-task']],
+                           {"nothing": "evergetreplaced"}))
+    assert get_key(output, "spec.params.0.value"), "{{value}}"
 
 
 @pytest.mark.skip(
